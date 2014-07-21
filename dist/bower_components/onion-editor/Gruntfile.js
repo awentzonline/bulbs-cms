d6e55a3cf10ed5b7a2c96aa9ce6e52e777eacba9
Michael Wnuk <mwnuk@theonion.com>
2014-07-21 15:43:27 -0500

    added dist, for testing

---
+++ dist/bower_components/onion-editor/Gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {
+  var banner = '/*! <%= pkg.name %> <%= grunt.template.today("yyyy-mm-dd") %> */\n';
+  // Configure Grunt
+  grunt.initConfig({
+    pkg: grunt.file.readJSON('package.json'),
+    requirejs: {
+      compile: {
+        options: {
+          mainConfigFile: 'build.js'
+        }
+      }
+    },
+    watch: {
+      scripts: {
+        files: ['src/js/*.js', 'src/js/*/*.js', 'src/less/*.less','src/less/*/*.less'],
+        tasks: ['requirejs', 'less'],
+      }
+    },
+    less: {
+      production: {
+        options: {
+          paths: ["src/less"],
+          cleancss: false
+        },
+        files: {
+          'build/editor-main.css': ['src/less/editor/*'],
+          'build/inline.css': ['src/less/inline.less']
+        }
+      }
+    }
+  });
+  // Load external tasks
+  grunt.loadNpmTasks('grunt-contrib-less');
+  grunt.loadNpmTasks('grunt-contrib-requirejs');
+  grunt.loadNpmTasks('grunt-contrib-watch');
+  grunt.registerTask('default', ['requirejs', 'less']);
+};
